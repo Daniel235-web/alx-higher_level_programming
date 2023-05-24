085cb7cc31b7ddd72271023e0b3ecd8895e7ed6f
Daniel235-web <samueledaniel235@gmail.com>
2023-05-25 00:03:39 +0100

    JS

--- 0x12-javascript-warm_up/100-let_me_const.js
+++ 0x12-javascript-warm_up/100-let_me_const.js
@@ -3,13 +3,21 @@
 const fs = require('fs');
 
 // Read the contents of the file
-const filePath = 'path/to/your/file.js';
-const fileContents = fs.readFileSync(filePath, 'utf8');
+fs.readFile('path/to/your/file.js', 'utf8', (err, data) => {
+  if (err) {
+    console.error(err);
+    return;
+  }
 
-// Modify the value of myVar to 333
-const modifiedContents = fileContents.replace(/const myVar = .+;/, 'const myVar = 333;');
+  // Modify the value of myVar to 333
+  const modifiedData = data.replace(/const myVar = \d+;/, 'const myVar = 333;');
 
-// Write the modified contents back to the file
-fs.writeFileSync(filePath, modifiedContents, 'utf8');
-
-console.log('Variable modified successfully!');
+  // Write the modified contents back to the file
+  fs.writeFile('path/to/your/file.js', modifiedData, 'utf8', (err) => {
+    if (err) {
+      console.error(err);
+      return;
+    }
+    console.log('Variable modified successfully!');
+  });
+});
