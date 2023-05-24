ad142c0006b2f0e0450c632c5cf5a04188b460ec
Daniel235-web <samueledaniel235@gmail.com>
2023-05-24 18:09:14 +0100

    Js

--- 0x12-javascript-warm_up/9-add.js
+++ 0x12-javascript-warm_up/9-add.js
@@ -1,21 +1,14 @@
 #!/usr/bin/node
 
-function add (a, b) {
-  return a + b;
+const args = process.argv;
+const A = parseInt(args[2]);
+const B = parseInt(args[3]);
+if (isNaN(A) || isNaN(B)) {
+  console.log('NaN');
+} else {
+  console.log(add(A, B));
 }
 
-const arg1 = process.argv[2];
-const arg2 = process.argv[3];
-
-const num1 = parseInt(arg1);
-const num2 = parseInt(arg2);
-
-console.log('First argument:', num1);
-console.log('Second argument:', num2);
-
-if (isNaN(num1) || isNaN(num2)) {
-  console.log('Invalid arguments');
-} else {
-  const result = add(num1, num2);
-  console.log('Addition:', result);
+function add (a, b) {
+  return (a + b);
 }
